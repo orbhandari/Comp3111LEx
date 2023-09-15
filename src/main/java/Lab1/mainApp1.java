7897f6000e8e9f89d96c354ce7113497987d9a1a
Om Bhandari <orbhandari@connect.ust.hk>
2023-09-15 11:16:57 +0800

    Added new print statements

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -4,6 +4,8 @@ public class mainApp1 {
     public static void main(String[] args) {
         int b = 2;
         int n = 1;
+        System.out.println("Welcome!");
+        System.out.println("Program start!");
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = "+myLibrary.factorial(n));
         System.out.println("Program ..Ended ...");
